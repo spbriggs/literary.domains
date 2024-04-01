7e27e61959165d54e6096852fcd69c76472cfb75
Simon Briggs <simonpbriggs@gmail.com>
2024-04-01 22:18:58 +0100

    Use SQLite for faster querying of Gutenberg catalogue

--- blueprints/search_gutenberg.py
+++ blueprints/search_gutenberg.py
@@ -1,13 +1,33 @@
 import os
 import re
 import csv
+from pathlib import Path
+import pandas
 from flask import Blueprint, request, jsonify
+import sqlite3
+from sqlite3 import Error
 
 search_gutenberg_blueprint = Blueprint('search_gutenberg_blueprint', __name__)
+gutenberg_db_file = f'data/gutenberg.sqlite'
+
+
+def init_gutenberg_search():
+    conn = None
+    try:
+        conn = sqlite3.connect(gutenberg_db_file)
+        df = pandas.read_csv('data/pg_catalog.csv')
+        df.to_sql('gutenberg', conn, if_exists='append', index=False)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
 
 
 @search_gutenberg_blueprint.route('/search-gutenberg')
 def search_gutenberg():
+    if not os.path.isfile(gutenberg_db_file):
+        init_gutenberg_search()
     # TODO: Check if the existing pg_catalog.csv file is older than one day and if so,
     # download it again from https://www.gutenberg.org/cache/epub/feeds/pg_catalog.csv
 
@@ -15,30 +35,18 @@ def search_gutenberg():
     result_ids = []
     results = []
 
-    with open('data/pg_catalog.csv', newline='') as pg_catalog:
-        reader = csv.DictReader(pg_catalog)
-        for book in reader:
-            book_authors = [re.sub(r", \d*-\d*", "", author).strip()
-                            for author in book["Authors"].split(";")]
+    conn = None
+    try:
+        conn = sqlite3.connect(gutenberg_db_file)
+        cur = conn.cursor()
+        results = cur.execute('select title, authors from gutenberg where title like ? or authors like ?',
+                              ('%' + query + '%', '%' + query + '%'))
+        stuff=results.fetchall()
+        mappedstuff = list(map(lambda x: {'title': x[0], 'authors': x[1].split(';')}, stuff))
+        return jsonify(results=mappedstuff)
 
-            book_authors = [re.sub(r" \[.*\]", "", author)
-                            for author in book_authors]
-
-            book_title = re.sub(r" \[.*\]", "", book["Title"].strip())
-
-            book_authors_lower = book_authors[0].lower()
-            book_title_lower = book_title.lower()
-
-            # if (query in book_authors_lower or query in book_title_lower):
-            if (all(word in book_authors_lower for word in query.split(" ")) or all(word in book_title_lower for word in query.split(" "))):
-                result_id = book_authors_lower + book_title_lower
-                if (not result_id in result_ids):
-                    result_ids.append(result_id)
-                    results.append({
-                        "title": book_title,
-                        "authors": book_authors
-                    })
-
-    return jsonify(
-        results=results
-    )
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
